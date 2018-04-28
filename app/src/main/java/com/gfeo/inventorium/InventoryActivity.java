bfcddda31ab1365693057aca6940b0e908fe58e7
Gabriel Feo <gabriel@gabrielfeo.com>
2018-04-28 02:31:36 -0300

    [InventoryActivity] Added a "New entry" FAB

--- app/src/main/java/com/gfeo/inventorium/InventoryActivity.java
+++ app/src/main/java/com/gfeo/inventorium/InventoryActivity.java
@@ -1,7 +1,9 @@
 package com.gfeo.inventorium;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class InventoryActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class InventoryActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_inventory);
+		setSupportActionBar((Toolbar) findViewById(R.id.inventory_toolbar));
+		setupFab();
+	}
+
+	private void setupFab() {
+		findViewById(R.id.inventory_fab).setOnClickListener(view -> {
+			Intent intent = new Intent(this, EditorActivity.class);
+			startActivity(intent);
+		});
 	}
 }
