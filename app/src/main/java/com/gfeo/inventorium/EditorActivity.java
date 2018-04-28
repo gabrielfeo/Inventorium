59427dcfc8f9461311095008eecfb2c6c632de39
Gabriel Feo <gabriel@gabrielfeo.com>
2018-04-28 19:19:08 -0300

    Quantity counter can be manually set now (EditText with editor action listener)

--- app/src/main/java/com/gfeo/inventorium/EditorActivity.java
+++ app/src/main/java/com/gfeo/inventorium/EditorActivity.java
@@ -1,15 +1,20 @@
 package com.gfeo.inventorium;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class EditorActivity extends AppCompatActivity {
 
 	private static int quantityCount;
-	private TextView quantityTextView;
-	public static final int DECREASE_QUANTITY = 0;
-	public static final int INCREASE_QUANTITY = 1;
+	private EditText quantityEditText;
+	private static final int DECREASE_QUANTITY = 0;
+	private static final int INCREASE_QUANTITY = 1;
+	private static final int SET_TO_INPUTTED_QUANTITY = 2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +25,23 @@ public class EditorActivity extends AppCompatActivity {
 	}
 
 	private void setupQuantityCounter() {
+		//TODO Change for "update" functionality
 		quantityCount = 0;
-		quantityTextView = findViewById(R.id.editor_textview_quantity);
+		quantityEditText = findViewById(R.id.editor_textview_quantity);
+		quantityEditText.setText(String.valueOf(quantityCount));
 		findViewById(R.id.editor_button_minus)
 				.setOnClickListener(view -> updateQuantityCount(DECREASE_QUANTITY));
 		findViewById(R.id.editor_button_plus)
 				.setOnClickListener(view -> updateQuantityCount(INCREASE_QUANTITY));
+		quantityEditText.setOnEditorActionListener(
+				(view, actionId, keyEvent) -> {
+					if (actionId == EditorInfo.IME_ACTION_DONE) {
+						updateQuantityCount(SET_TO_INPUTTED_QUANTITY);
+						view.clearFocus();
+						hideSoftKeyboard(view);
+						return true;
+					} else { return false; }
+				});
 	}
 
 	private void updateQuantityCount(int operation) {
@@ -33,8 +49,18 @@ public class EditorActivity extends AppCompatActivity {
 			quantityCount--;
 		} else if (operation == INCREASE_QUANTITY) {
 			quantityCount++;
+		} else if (operation == SET_TO_INPUTTED_QUANTITY) {
+			quantityCount = Integer.valueOf(quantityEditText.getText().toString());
 		} else { return; }
-		quantityTextView.setText(String.valueOf(quantityCount));
+		quantityEditText.setText(String.valueOf(quantityCount));
+	}
+
+	private void hideSoftKeyboard(View requestingView) {
+		InputMethodManager imm =
+				(InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		if (imm != null) {
+			imm.hideSoftInputFromWindow(requestingView.getWindowToken(), 0);
+		}
 	}
 
 }
