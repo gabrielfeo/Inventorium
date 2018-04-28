a0b4905f626bb213d74a4132421a4173c9cca454
Gabriel Feo <gabriel@gabrielfeo.com>
2018-04-28 01:27:23 -0300

    Renamed MainActivity

--- app/src/main/java/com/gfeo/inventorium/InventoryActivity.java
+++ app/src/main/java/com/gfeo/inventorium/InventoryActivity.java
@@ -3,11 +3,11 @@ package com.gfeo.inventorium;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class InventoryActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_inventory);
 	}
 }
